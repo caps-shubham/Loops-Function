2a6644203859836aeecf4ba3fdad380f2d93a46e
Shubham Akash <shubhamakash30@gmail.com>
2026-08-31 22:32:10 +0530

    js loops & function

---
+++ function.js
@@ -0,0 +1,111 @@
+// 11. Create a function named greetUser(name) that takes a name as a parameter and displays a greeting
+// message.
+// Example:
+// Input: Rahul
+// Output: Hello, Rahul
+
+// function greetUser(name) {
+//     console.log("Hello,  " + name);
+// }
+// greetUser("Rahul");
+
+
+
+// 12. Create a function that takes two numbers as parameters and returns their sum.
+ 
+// function sumOfTwo(num1 , num2) {
+//     return num1 + num2;
+// }
+//   console.log(sumOfTwo(2,5));
+  
+
+
+// 13.Create a function that takes a number and checks whether it is even or odd.
+
+// function checkEvenOdd(num) {
+//     if (num % 2 === 0) {
+//         return "Even" ;
+//     } else {
+//         return "Odd" ;
+//     } 
+        
+// }
+// console.log(checkEvenOdd(5));
+  
+
+
+// 14. Create a function that takes a number and returns its square.
+// function squareOf(num) {
+//     return num ** 2
+// }
+// console.log(squareOf(71));
+
+
+
+// 15.Create a function that takes two numbers and returns the greater number.
+// function findGreater(num1 , num2) {
+//     if (num1 > num2) {
+//         return num1 ;
+//     }
+//     else 
+//         return num2 ;
+// }
+
+// console.log(findGreater(7,92));
+
+
+
+//16.Create a function named calculateTotal(price, quantity) using a function declaration. The function
+// should calculate and display the total price.
+// Example:
+// Input: price = 100, quantity = 3
+// Output: Total Price: 300
+
+// function calculateTotal(price, quantity) {
+//     return price * quantity;
+// }
+//  console.log("Total Price:" + calculateTotal(100,3));
+ 
+
+
+//17.Create a function printNumbers(n) that prints numbers from 1 to n using a loop. 
+// Example: 
+// Input: 5 
+// Output: 1 2 3 4 5 
+
+// function printNumbers(n) {
+//     let num = n
+//     for (let i = 1 ; i <= num; i++) {
+//         console.log(i)
+//     }
+// }
+
+// printNumbers(5);
+
+
+
+// 18.Create a function printTable(num) that prints the multiplication table of the given number. 
+
+// function printTable(num) {
+//     for (let i=1; i<=10; i++) {
+//         console.log(num + " x " + i + " = " + (num*i));
+//     }
+// }
+// printTable(20);
+
+
+
+//19. Create a function sumNumbers(n) that calculates and returns the sum of numbers from 1 to n. 
+// Example: 
+// Input: 5 
+// Output: 15 
+
+// function sumNumbers(n) {
+//     let num = n
+//     let sum = 0
+//     for (let i=1; i<=num; i++){
+//       sum = sum + i;
+//    }
+// return sum ;
+// }
+// console.log(sumNumbers(5));
---
+++ loops.js
@@ -0,0 +1,94 @@
+// 1.Write a program to print numbers from 1 to 10 using a for loop.
+//   let num = 10 
+//   for ( let i = 1; i<=num; i++) {
+//   console.log(i)
+//  }
+
+
+// 2.Write a program to print all even numbers from 1 to 20.
+// let num = 20
+// for (let i=1; i<=num; i++) {
+//     if (i % 2 == 0)
+//     console.log(i);
+// }
+
+
+//  3.Write a program to print all odd numbers from 1 to 20. 
+// let num = 20
+// for (let i = 1; i <= num; i++) {
+//     if (i % 2 != 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// 4.Write a program to print numbers from 10 to 1 using a loop. 
+// let num = 1
+// for (let i = 10; i>=num; i--) {
+//     console.log(i);
+// }
+
+
+// 5.Write a program to calculate the sum of numbers from 1 to 10.
+// let num = 10;
+// let sum = 0;
+// for (let i = 1; i <= num; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// 6.Take a number and print its multiplication table up to 10. 
+// let num = 10;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(num * i);
+// }
+
+
+// 7. Write a program to print numbers from 1 to 10 using a while loop.
+//   let num = 10
+//   let i=1
+//   while (i<=10)  {
+//     console.log(i);
+//     i++;
+//   }
+
+
+// 8.Write a program to calculate the sum of all even numbers from 1 to 20.
+
+// let num = 20;
+// let i = 1;
+// let sum = 0;
+
+// while (i <= 20) {
+//     if (i % 2 == 0) {
+//         sum = sum + i;
+//     }
+//     i++;
+// }
+// console.log(sum);
+
+
+// 9.Write a program using a while loop to print numbers from 1 onwards, but stop the loop when the 
+// number reaches 6 using the break statement. 
+// let num = 10;
+// let i = 1;
+// while (i<=num) {
+//     if(i === 6)
+//         break;
+// console.log(i);
+// i++
+// }
+
+
+// 10.Print numbers from 1 to 10, but skip the number 5 using the continue statement.
+// let num = 10
+// let i = 1
+// while (i <= num) {
+//     if (i == 5) {
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+// }
